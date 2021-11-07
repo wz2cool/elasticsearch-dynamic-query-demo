e1e2ca32808577c0d866f11f5c16fffc59c61cd3
wz2cool <wz2cool@live.cn>
2021-11-07 11:30:14 +0800

    init

---
+++ src/main/java/com/github/wz2cool/elasticsearchdynamicquerydemo/ElasticsearchDynamicQueryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.elasticsearchdynamicquerydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDynamicQueryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDynamicQueryDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wz2cool/elasticsearchdynamicquerydemo/ElasticsearchDynamicQueryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.elasticsearchdynamicquerydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchDynamicQueryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
